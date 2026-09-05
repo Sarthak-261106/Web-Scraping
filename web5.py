18642d684f4a3307c82c2b168c984a6852f8a252
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-09-05 16:32:45 +0530

    Update web5.py

--- web5.py
+++ web5.py
@@ -1,12 +1,30 @@
+# import requests
+# from bs4 import BeautifulSoup
+#
+#
+#
+# def Extract(url):
+#     response = requests.get(url=url).content
+#     soup = BeautifulSoup(response, 'lxml')
+#     tag = soup.find('id',{'id':'no-right'})
+#     h=tag.find('h2')
+#     print(h)
+#
+# Extract(url='https://en.wikipedia.org/wiki/Wikipedia')
+
 import requests
 from bs4 import BeautifulSoup
 
 
-
 def Extract(url):
-    response = requests.get(url=url).content
-    soup = BeautifulSoup(response, 'lxml')
-    tag = soup.td
-    print(tag)
+    response = requests.get(url).content
+
+    soup = BeautifulSoup(response, features='lxml')
+
+    tag = soup.find(id='no-right')
+    h = tag.find('h2')
+
+    print(h)
+
 
 Extract(url='https://en.wikipedia.org/wiki/Wikipedia')
