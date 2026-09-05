5b5af6b89ee76357310deafe7c91e1f0a52d0a1a
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-09-05 16:55:29 +0530

    csv

--- web5.py
+++ web5.py
@@ -11,20 +11,45 @@
 #     print(h)
 #
 # Extract(url='https://en.wikipedia.org/wiki/Wikipedia')
+#
+# import requests
+# from bs4 import BeautifulSoup
+# import csv
+#
+# def Extract(url):
+#     response = requests.get(url).content
+#
+#     soup = BeautifulSoup(response, features='lxml')
+#
+#     tag = soup.find(id='no-right')
+#     h = tag.find_all('h2')
+#     content = [span.text for span in h]
+#
+#     with open('wiki.csv','w') as csvfile:
+#         csv_write = csv.writer(csvfile)
+#         csv_write.writerow(content)
+#
+#
+# Extract(url='https://en.wikipedia.org/wiki/Main_Page')
 
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 
 def Extract(url):
-    response = requests.get(url).content
+    response = requests.get(url=url).content
 
-    soup = BeautifulSoup(response, features='lxml')
+    soup = BeautifulSoup(response, 'lxml')
 
-    tag = soup.find(id='no-right')
-    h = tag.find_all('h2')
+    h = soup.find_all('h2')
+
     content = [span.text for span in h]
-    print(content)
+
+    with open('wiki.csv', 'w', ) as csvfile:
+        csv_writer = csv.writer(csvfile)
+
+        csv_writer.writerow(content)
 
 
 Extract(url='https://en.wikipedia.org/wiki/Main_Page')
