de2f92983b17e4bfc1f35194fba9360fbfad8205
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-09-05 15:48:26 +0530

    Update web.py

--- web.py
+++ web.py
@@ -6,9 +6,17 @@
 #
 # print(ord('@'))
 
-import urllib.request, urllib.parse, urllib.error
+# import urllib.request, urllib.parse, urllib.error
+#
+# url = urllib.request.urlopen('file:///C:/Users/sarth/OneDrive/Desktop/index1.html')
+#
+# for line in url:
+#     print(line.decode().strip())
 
-url = urllib.request.urlopen('file:///C:/Users/sarth/OneDrive/Desktop/index1.html')
+import requests
 
-for line in url:
-    print(line.decode().strip())
+url='https://en.wikipedia.org/wiki/Dog'
+
+response = requests.get(url=url)
+print(dir(response))
+print(response.request.headers)
